1aa6f280b26a554ece61060b84d1fa7d72397c38
flaviodrummond <flaviosax20@gmail.com>
2024-02-12 13:27:38 -0300

    iniciando o back-end do projeto code-burguer

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const { Router, request, response } = require("express") /* só vamos importar o express que é responsável pelas rotas que é o Router */
+
+const routes = new Router() /* armazenamos nosso Router dentro de uma variável */
+
+/* agora criamos nossa primeira rota, para saber se está tudo funcionando */
+
+routes.get('/', (request, response) => {
+    return response.json({message: "Hello World"})
+})
+
+module.exports = routes
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const app = require("./app") /* damos um const app para importar a class que estamos exportando lá no app.js */
+
+app.listen(3000)
