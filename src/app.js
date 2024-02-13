2da69af303d193ed0ee903487314f2c2a2e0f00a
flaviodrummond <flaviosax20@gmail.com>
2024-02-13 08:35:28 -0300

    instalando a padronização dos códigos, esLint e Prettier

--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
-import express from "express" /* importando o express */ 
+import express from 'express' /* importando o express */
 import routes from './routes' /* importando as nossas rotas */
 
 class App { /* criamos nossa class app e dentro dela começamos com o método constructor, (lembrando que toda vez que estâncio minha class com o new app. O método constructor é chamado) */
-    constructor() { /* dentro do constructor, deixo meu express disponível, dando um this. app */
-        this.app = express()
+  constructor () { /* dentro do constructor, deixo meu express disponível, dando um this. app */
+    this.app = express()
 
-        /* Toda vez que minha aplicação começar a rodar, preciso que minha rota e meu middlewares, comece a rodar. Por isso trago os middlewares e as routes para dentro do meu constructor */
+    /* Toda vez que minha aplicação começar a rodar, preciso que minha rota e meu middlewares, comece a rodar. Por isso trago os middlewares e as routes para dentro do meu constructor */
 
-        this.middlewares()
-        this.routes()
+    this.middlewares()
+    this.routes()
+  }
 
-    }
+  /* Dentro da minha class, ainda crio dois métodos, que é o middlewares e as routes */
 
-    /* Dentro da minha class, ainda crio dois métodos, que é o middlewares e as routes */
+  middlewares () { /* no express, eu preciso avisar que vamos usar o json */
+    this.app.use(express.json())
+  }
 
-    middlewares() { /* no express, eu preciso avisar que vamos usar o json */
-        this.app.use(express.json())
-
-    }
-
-    routes() {
-        this.app.use(routes)
-    }
+  routes () {
+    this.app.use(routes)
+  }
 }
 
 export default new App().app /* agora exportando o meu app */
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-import { Router } from "express" /* só vamos importar o express que é responsável pelas rotas que é o Router */
+import { Router } from 'express' /* só vamos importar o express que é responsável pelas rotas que é o Router */
 
 const routes = new Router() /* armazenamos nosso Router dentro de uma variável */
 
 /* agora criamos nossa primeira rota, para saber se está tudo funcionando */
 
 routes.get('/', (request, response) => {
-    return response.json({message: "Hello World"})
+  return response.json({ message: 'Hello World' })
 })
 
 export default routes
