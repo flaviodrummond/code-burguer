c2791eac2fda0271c4f19756ee84e17741b7903c
flaviodrummond <flaviosax20@gmail.com>
2024-02-20 12:34:15 -0300

    Criado nosso Controller de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,11 @@
 import { Router } from "express" /* só vamos importar o express que é responsável pelas rotas que é o Router */
-import { v4 } from "uuid"
-import User from "./app/models/User"
+import UserConstroller from "./app/controllers/UserConstroller"
 
 const routes =
   new Router() /* armazenamos nosso Router dentro de uma variável */
 
 /* agora criamos nossa primeira rota, para saber se está tudo funcionando */
 
-routes.get("/", async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: "Flávio Drumond",
-    email: "flaviodrumonnd@gmail.com",
-    password_hash: "134856",
-  })
-  return response.json(user)
-})
+routes.post("/users", UserConstroller.store)
 
 export default routes
