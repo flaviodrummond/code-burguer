e637ca1282b82b841ea91df7bc6eb1c5c2b628f1
flaviodrummond <flaviosax20@gmail.com>
2024-02-26 13:13:24 -0300

    Criado nossa rota de produtos e nosso controller

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express" /* só vamos importar o express que é responsável pelas rotas que é o Router */
 import UserConstroller from "./app/controllers/UserConstroller"
 import SessionController from "./app/controllers/SessionController"
+import ProductController from "./app/controllers/ProductController"
 
 const routes =
   new Router() /* armazenamos nosso Router dentro de uma variável */
@@ -10,5 +11,7 @@ const routes =
 routes.post("/users", UserConstroller.store)
 
 routes.post("/sessions", SessionController.store)
+
+routes.post("/products", ProductController.store)
 
 export default routes
